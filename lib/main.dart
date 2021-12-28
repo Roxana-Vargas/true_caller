2e60c0c9b9a5bb796f9d9263b6243b9f07d4754e
Roxana Vargas <roxvargasc@gmail.com>
2021-12-28 18:23:56 -0500

    welcome view layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:my_app/pages/call_simulator.dart';
 
-void main() => runApp(const MaterialApp(home: MyApp()));
+void main() =>
+    runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -11,27 +14,94 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome'),
-        ),
-        body: Center(
-          child: Column(
+          backgroundColor: const Color.fromARGB(255, 136, 71, 242),
+          body: Column(
             children: [
-              const Text('Home'),
-              TextButton(
-                  onPressed: () {
-                    debugPrint('start');
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const CallSimulator(),
-                        ));
-                  },
-                  child: const Text('Start')),
+              logo(context),
+              Center(
+                child: Column(
+                  children: [
+                    imageWelcome(context),
+                    titleWelcome(context),
+                    subtitleWelcome(context),
+                    buttonStart(context)
+                  ],
+                ),
+              ),
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
+}
+
+Widget logo(context) {
+  return Container(
+    alignment: Alignment.topLeft,
+    margin: const EdgeInsets.only(top: 60.0, left: 30.0),
+    child: SizedBox(
+      width: MediaQuery.of(context).size.width * 0.25,
+      child: Image.asset('images/logo.png'),
+    ),
+  );
+}
+
+Widget imageWelcome(context) {
+  return Container(
+      margin: EdgeInsets.symmetric(
+          horizontal: 30.0, vertical: MediaQuery.of(context).size.height * 0.1),
+      child: SvgPicture.asset(
+        'assets/images/phone_call.svg',
+        height: MediaQuery.of(context).size.height * 0.25,
+      ));
+}
+
+Widget titleWelcome(context) {
+  return Container(
+      margin: const EdgeInsets.only(bottom: 20.0),
+      child: Text(
+        'True Caller k',
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: MediaQuery.of(context).size.height * 0.03,
+            fontWeight: FontWeight.w600),
+      ));
+}
+
+Widget subtitleWelcome(context) {
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 30.0),
+    child: Text(
+      'Identify who is calling you and decide whether to attend them or not. also receive information about the best offers!',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: MediaQuery.of(context).size.height * 0.02,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget buttonStart(context) {
+  return Container(
+    width: MediaQuery.of(context).size.width * 0.85,
+    height: MediaQuery.of(context).size.height * 0.05,
+    margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.15),
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          shape: const StadiumBorder(),
+          primary: Colors.white,
+          onPrimary: const Color.fromARGB(255, 136, 71, 242)),
+      onPressed: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const CallSimulator(),
+            ));
+      },
+      child: const Text(
+        'Get Start',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    ),
+  );
 }
