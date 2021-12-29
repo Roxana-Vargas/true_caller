7ca869f8583d8c03cbfdbfb55718492a83e67e3a
Roxana Vargas <roxvargasc@gmail.com>
2021-12-29 11:54:46 -0500

    Fix call simulator view

--- lib/pages/call_simulator.dart
+++ lib/pages/call_simulator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:flutter_incoming_call/flutter_incoming_call.dart';
 import 'package:my_app/models/callcenter_agent.dart';
@@ -75,126 +76,175 @@ class _CallSimulatorState extends State<CallSimulator> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: Stack(
-        children: [
-          Container(),
-          Container(
-            height: 600.0,
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: NetworkImage(
-                  'https://cdn1.vectorstock.com/i/1000x1000/69/15/call-center-isometric-3d-concept-vector-23576915.jpg'),
-              fit: BoxFit.cover,
-            )),
-          ),
-          Positioned(
-            top: 500.0,
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: Container(
-              padding: const EdgeInsets.all(35.0),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  const Text(
-                    'Call Simulator',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w500,
+          backgroundColor: const Color.fromARGB(255, 136, 71, 242),
+          body: Stack(
+            children: [
+              Container(),
+              imageCallSimulator(context),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.43,
+                left: 0.0,
+                right: 0.0,
+                bottom: 0.0,
+                child: Container(
+                  padding:
+                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
                     ),
+                    color: Colors.white,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(40.0),
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        'https://www.tu-voz.com/wp-content/uploads/2019/07/callcenter-telemarketing.jpg',
+                  child: Column(
+                    children: [
+                      title(context),
+                      subtitle(context),
+                      avatar(context),
+                      Container(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height * 0.025),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Button(
+                                colorButton:
+                                    const Color.fromARGB(255, 71, 184, 76),
+                                callAction: () {
+                                  _incomingCall('51920234549');
+                                },
+                                iconPhone: const Icon(Icons.call)),
+                            Button(
+                                colorButton:
+                                    const Color.fromARGB(255, 248, 234, 34),
+                                callAction: () {
+                                  Future.delayed(const Duration(seconds: 5),
+                                      () {
+                                    _incomingCall('51918604749');
+                                  });
+                                },
+                                iconPhone: const Icon(Icons.add_ic_call)),
+                            Button(
+                              colorButton:
+                                  const Color.fromARGB(255, 204, 53, 53),
+                              callAction: () {
+                                _endAllCalls();
+                                showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('Name'),
+                                        content: const Text('Offer'),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                debugPrint('Yes');
+                                              },
+                                              child: const Text('Yes')),
+                                          TextButton(
+                                              onPressed: () {
+                                                debugPrint('No');
+                                              },
+                                              child: const Text('No')),
+                                        ],
+                                      );
+                                    });
+                              },
+                              iconPhone: const Icon(Icons.call_end),
+                            ),
+                          ],
+                        ),
                       ),
-                      radius: 80.0,
-                    ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        FloatingActionButton(
-                          heroTag: 'btn1',
-                          child: const Icon(Icons.call),
-                          elevation: 15.0,
-                          backgroundColor: Colors.green,
-                          onPressed: () {
-                            _incomingCall('51918604749');
-                          },
-                        ),
-                        FloatingActionButton(
-                          heroTag: 'btn2',
-                          child: const Icon(Icons.add_ic_call),
-                          elevation: 15.0,
-                          backgroundColor: Colors.amber,
-                          onPressed: () {
-                            Future.delayed(const Duration(seconds: 5), () {
-                              _incomingCall('51918604749');
-                            });
-                          },
-                        ),
-                        FloatingActionButton(
-                          heroTag: 'btn3',
-                          child: const Icon(Icons.call_end),
-                          elevation: 15.0,
-                          backgroundColor: Colors.red,
-                          onPressed: () {
-                            _endAllCalls();
-                            showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    title: const Text('Name'),
-                                    content: const Text('Offer'),
-                                    actions: [
-                                      TextButton(
-                                          onPressed: () {
-                                            debugPrint('Yes');
-                                          },
-                                          child: const Text('Yes')),
-                                      TextButton(
-                                          onPressed: () {
-                                            debugPrint('No');
-                                          },
-                                          child: const Text('No')),
-                                    ],
-                                  );
-                                });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+              SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.all(25.0),
+                  width: 130.0,
+                  child: Image.network(
+                      'https://www.grupokonecta.com/wp-content/uploads/2016/06/logo.png'),
+                ),
+              )
+            ],
           ),
-          SafeArea(
-            child: Container(
-              padding: const EdgeInsets.all(25.0),
-              width: 130.0,
-              child: Image.network(
-                  'https://www.grupokonecta.com/wp-content/uploads/2016/06/logo.png'),
-            ),
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
 
+Widget imageCallSimulator(context) {
+  return (Container(
+    padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.05),
+    height: MediaQuery.of(context).size.height * 0.45,
+    width: MediaQuery.of(context).size.width,
+    child: SvgPicture.asset(
+      'images/image_call_simulator.svg',
+    ),
+  ));
+}
+
+Widget title(context) {
+  return (Text(
+    'Call Simulator',
+    style: TextStyle(
+      fontSize: MediaQuery.of(context).size.height * 0.03,
+      fontWeight: FontWeight.w500,
+    ),
+  ));
+}
+
+Widget subtitle(context) {
+  return Container(
+    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+    child: (Text(
+      'The person who will call you is Yudith',
+      style: TextStyle(
+        fontSize: MediaQuery.of(context).size.height * 0.02,
+        fontWeight: FontWeight.w300,
+      ),
+    )),
+  );
+}
+
+Widget avatar(context) {
+  return (Padding(
+    padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
+    child: CircleAvatar(
+      backgroundImage: const NetworkImage(
+        'https://www.tu-voz.com/wp-content/uploads/2019/07/callcenter-telemarketing.jpg',
+      ),
+      radius: MediaQuery.of(context).size.height * 0.09,
+    ),
+  ));
+}
+
+class Button extends StatelessWidget {
+  final VoidCallback callAction; // Notice the variable type
+  final Color colorButton;
+  final Icon iconPhone;
+
+  const Button({
+    Key? key,
+    required this.callAction,
+    required this.colorButton,
+    required this.iconPhone,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      heroTag: 'btn1',
+      child: iconPhone,
+      elevation: 15.0,
+      backgroundColor: colorButton,
+      onPressed: callAction,
+    );
+  }
+}
 /*
 class Alert extends StatelessWidget {
   final String title, description, buttonText;
