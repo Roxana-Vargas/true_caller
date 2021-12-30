18d8d1e7c2c83e7d2367636675501a364697f345
Roxana Vargas <roxvargasc@gmail.com>
2021-12-30 12:12:08 -0500

    Fix function name

--- lib/utils/incoming_call.dart
+++ lib/utils/incoming_call.dart
@@ -8,7 +8,7 @@ void incomingCall(String number) {
   const avatar =
       'https://scontent.fhel6-1.fna.fbcdn.net/v/t1.0-9/62009611_2487704877929752_6506356917743386624_n.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=cIgJjOYlVj0AX_J7pnl&_nc_ht=scontent.fhel6-1.fna&oh=ef2b213b74bd6999cd74e3d5de235cf4&oe=5F6E3331';
   const type = HandleType.generic;
-  const handle = 'Incomin call example';
+  const handle = 'Incoming call example';
   const isVideo = true;
   FlutterIncomingCall.displayIncomingCall(
       uid, number, avatar, handle, type, isVideo);
